169cc5531971f28f634a9a5192beb6d535fbe45d
ColleenSchmidt <44813965+ColleenSchmidt@users.noreply.github.com>
2019-12-02 11:56:12 -0500

    Add files via upload

---
+++ src/HashTableTest.java
@@ -0,0 +1,44 @@
+/**
+ * test class for hash table
+ * @author colle
+ *
+ */
+public class HashTableTest extends student.TestCase {
+
+    //hash table to test with
+    private HashTable table;
+    
+    /**
+     * set up method
+     */
+    public void setUp() throws Exception {
+        table = new HashTable<String, Integer>();
+        
+    }
+    
+    /**
+     * tests the size method
+     */
+    public void testSize() {
+        assertEquals(0, table.size());
+    }
+    
+    /**
+     * tests the is empty method
+     */
+    public void testisEmpty() {
+        assertTrue(table.isEmpty());
+    }
+    
+    /**
+     * tests the add method
+     */
+    public void testadd() {
+        table.add("cat", 4);
+        assertEquals(1, table.size());
+    }
+
+
+    
+
+}
