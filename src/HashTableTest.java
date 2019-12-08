77c94e1d1af24a56bbde69af70654f83097014d3
ajd29 <44148153+ajd29@users.noreply.github.com>
2019-12-07 20:04:57 -0500

    Update HashTableTest.java

--- src/HashTableTest.java
+++ src/HashTableTest.java
@@ -1,14 +1,11 @@
-import student.TestCase;
 
 /**
  * test class for hash table
- * 
- * @author collee57 Colleen Schmidt
- * @author ajd29 Allison DeSantis
- * @version Dec 6 2019
+ * @author collee57
+ * @author ajd29
  *
  */
-public class HashTableTest extends TestCase {
+public class HashTableTest extends student.TestCase {
 
     // hash table to test with
     private HashTable<String, Integer> table;
@@ -20,43 +17,28 @@ public class HashTableTest extends TestCase {
     private Student s1;
     private Student s2;
 
-
     /**
      * Set up method
      */
     public void setUp() throws Exception {
         table = new HashTable<String, Integer>(32);
         studentTable = new HashTable<String, Student>(64);
-        s1 = new Student("123", "Sally student");
-        s2 = new Student("1234", "s2 student");
+        s1 = new Student("123", "Sally", "student");
+        s2 = new Student("1234", "s2", "student");
     }
-
 
     /**
      * Tests size()
-     * 
      * @throws Exception
      */
     public void testSize() throws Exception {
         setUp();
-        // empty
         assertEquals(0, table.size());
         assertEquals(0, studentTable.size());
-        // add one student
-        studentTable.add("123", s1);
-        assertEquals(1, studentTable.size());
-        // add another
-        studentTable.add("1234", s2);
-        assertEquals(2, studentTable.size());
-        // remove one
-        studentTable.remove("123");
-        assertEquals(1, studentTable.size());
     }
-
 
     /**
      * Tests get()
-     * 
      * @throws Exception
      */
     public void testGet() throws Exception {
@@ -65,37 +47,60 @@ public class HashTableTest extends TestCase {
         // not there
         assertNull(table.get("1"));
 
-        studentTable.add("123", s1);
-        // studentTable.add("123", s1);
-        // studentTable.add("123", s1);
-        studentTable.add("1234", s2);
-        assertEquals(studentTable.get("123"), s1);
-        // assertEquals(studentTable.get("1"), s1);
-        assertEquals(studentTable.get("1234"), s2);
+        studentTable.add("1", s1);
+        studentTable.add("2", s1);
+        studentTable.add("3", s1);
+        studentTable.add("4", s2);
+        assertEquals(studentTable.get("2").toString(), s1.toString());
+        assertEquals(studentTable.get("1").toString(), s1.toString());
+        assertEquals(studentTable.get("4").toString(), s2.toString());
     }
 
+    /**
+     * Tests the getSfoldKey() method
+     * @throws Exception
+     */
+    public void testGetSfoldKey() throws Exception {
+        setUp();
+        assertEquals(studentTable.getSfoldKey("36584246"), 25);
+        assertEquals(studentTable.getSfoldKey("477937023"), 52);
+    }
 
     /**
      * Tests isEmpty()
-     * 
      * @throws Exception
      */
     public void testIsEmpty() throws Exception {
         setUp();
         assertTrue(table.isEmpty());
         assertTrue(studentTable.isEmpty());
-        studentTable.add("3", s1);
-        assertFalse(studentTable.isEmpty());
-
     }
 
+    /**
+     * Tests isBucketFull()
+     * @throws Exception
+     */
+    public void testIsBucketFull() throws Exception {
+        setUp();
+        assertFalse(table.isBucketFull());
+
+        // fill bucket
+        String pid = "123";
+        for (int i = 0; i < 32; i++) {
+            studentTable.add(pid, s1);
+            pid += "1";
+        }
+        studentTable.add("1245", s1);
+        assertEquals(studentTable.size(), 33);
+        assertTrue(studentTable.isBucketFull());
+    }
 
     /**
      * Tests isFull()
+     * @throws Exception
      */
     public void testIsFull() throws Exception {
         setUp();
-        //empty
         assertFalse(table.isFull());
 
         // fill hash table
@@ -107,10 +112,8 @@ public class HashTableTest extends TestCase {
         assertTrue(table.isFull());
     }
 
-
     /**
      * Tests add()
-     * 
      * @throws Exception
      */
     public void testAdd() throws Exception {
@@ -127,13 +130,13 @@ public class HashTableTest extends TestCase {
 
         studentTable.add("123", s1);
         assertEquals(1, studentTable.size());
-        assertEquals("123, Sally Student at slot ", studentTable.get("123")
-            .toString());
+        assertEquals("123, Sally Student, score = 0",
+            studentTable.get("123").toString());
 
         studentTable.add("1234", s2);
         assertEquals(2, studentTable.size());
-        assertEquals("1234, S2 Student at slot ", studentTable.get("1234")
-            .toString());
+        assertEquals("1234, S2 Student, score = 0",
+            studentTable.get("1234").toString());
 
         // fill hash table
         String pid = "123";
@@ -151,12 +154,14 @@ public class HashTableTest extends TestCase {
         }
         studentTable.add("12", s1);
         assertEquals(studentTable.size(), 64);
-    }
 
+        // add one that exists, doesn't add
+        studentTable.add("12", s1);
+        assertEquals(studentTable.size(), 64);
+    }
 
     /**
      * Tests remove()
-     * 
      * @throws Exception
      */
     public void testRemove() throws Exception {
@@ -180,53 +185,5 @@ public class HashTableTest extends TestCase {
         // already removed
         studentTable.remove("1");
         assertEquals(studentTable.size(), 1);
-    }
-
-
-    /**
-     * tests the getsfoldkey method
-     * 
-     * @throws Exception
-     */
-    public void testgetSfoldKey() throws Exception {
-        setUp();
-
-        studentTable.add("123", s1);
-        int key = studentTable.getSfoldKey("123");
-        assertEquals(key, studentTable.getSfoldKey("123"));
-        System.out.println(key);
-    }
-
-
-    /**
-     * tests bucket full method
-     * 
-     * @throws Exception
-     */
-    public void testisBucketFull() throws Exception {
-        setUp();
-
-        assertFalse(studentTable.isBucketFull());
-
-        String pid = "123";
-        for (int i = 0; i < 30; i++) {
-            studentTable.add(pid, s1);
-            pid += "1";
-        }
-
-        System.out.println(studentTable.getSlotsFull());
-        // assertTrue(studentTable.isBucketFull());
-    }
-    
-    /**
-     * tests the get slots full method
-     * @throws Exception 
-     */
-    public void testgetSlotsFull() throws Exception {
-        setUp();
-        assertEquals(0, studentTable.getSlotsFull());
-        studentTable.add("123", s1);
-        studentTable.add("1234", s2);
-        assertEquals(2, studentTable.getSlotsFull());
     }
 }
