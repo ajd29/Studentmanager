b2e5e06a26d33f682c6fa8ba3cab399a086dc491
ajd29 <44148153+ajd29@users.noreply.github.com>
2019-12-10 14:41:05 -0500

    Update Reader.java

--- src/Reader.java
+++ src/Reader.java
@@ -1,6 +1,5 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.util.Scanner;
 
 /**
@@ -12,81 +11,104 @@ import java.util.Scanner;
  */
 public class Reader
 {
-    // StudentstuManager object
+    // StudentDatabase object
     private StudentDatabase stuManager;
+    private Scanner         file;
+
 
     /**
      * Creates a new reader object
-     * @param hashSize size of hash table
      *
+     * @param hashSize
+     *            size of hash table
+     * @param memFile
+     *            memory file name
      * @throws FileNotFoundException
      */
-    public Reader(int hashSize)
+    public Reader(String hashSize, String memFile)
         throws FileNotFoundException
     {
-        stuManager = new StudentDatabase(hashSize);
-
+        stuManager = new StudentDatabase(hashSize, memFile);
     }
 
+
+    /**
+     * Returns student database object
+     *
+     * @return StudentDatabase
+     */
+    public StudentDatabase getStudentManager()
+    {
+        return stuManager;
+    }
 
     /**
      * Reads input file
      *
      * @param fileName
      *            of file to read in
-     * @throws IOException
+     * @throws Exception
      */
     public void readCommands(String fileName)
-        throws IOException
+        throws Exception
     {
-        File newFile = new File(fileName);
-        Scanner file = new Scanner(newFile);
+        File commandFile = new File(fileName);
+        file = new Scanner(commandFile);
 
         while (file.hasNextLine())
         {
             String line = file.nextLine().trim();
+
             String[] input = line.split("\\s+");
 
-            if (input[0].equals("loadstudentdata"))
+            if (!line.isEmpty())
             {
-                stuManager.loadStudentData(input[1]);
-            }
-            else if (input[0].equals("insert"))
-            {
-                stuManager.insert(input[1], input[2], input[3]);
-            }
-            else if (input[0].equals("update"))
-            {
-                stuManager.update(input[1], input[2], input[3]);
-            }
-            else if (input[0].equals("essay"))
-            {
-                String essayLine = file.nextLine().trim();
-
-                String essay = "";
-
-                // while essay off has not been reached
-                while (!essayLine.startsWith("essay")) {
-                    essay += essayLine;
-                    essayLine = file.nextLine().trim();
+                if (input[0].equals("loadstudentdata"))
+                {
+                    stuManager.loadStudentData(input[1]);
                 }
-                stuManager.essay(essay);
-            }
-            else if (input[0].equals("remove"))
-            {
-                stuManager.remove(input[1]);
-            }
-            else if (input[0].equals("search"))
-            {
-                stuManager.search(input[1]);
-            }
-            else if (input[0].equals("clear"))
-            {
-                stuManager.clear(input[1]);
-            }
-            else if (input[0].equals("print"))
-            {
-                stuManager.print();
+                else if (input[0].equals("insert"))
+                {
+                    stuManager.insert(input[1], input[2], input[3]);
+                }
+                else if (input[0].equals("update"))
+                {
+                    stuManager.update(input[1], input[2], input[3]);
+                }
+                else if (input[0].equals("essay"))
+                {
+                    String essay = file.nextLine() + System.lineSeparator();
+
+                    String essayLine = file.nextLine();
+
+                    // while essay off has not been reached
+                    while (!essayLine.startsWith("essay off"))
+                    {
+                        essay += (essayLine + System.lineSeparator());
+                        essayLine = file.nextLine();
+                    }
+
+                    // remove last character
+                    essay.replace(essay.substring(essay.length() - 1), "");
+
+                    stuManager.essay(essay);
+                }
+                else if (input[0].equals("remove"))
+                {
+                    stuManager.remove(input[1]);
+                }
+                else if (input[0].equals("search"))
+                {
+                    stuManager.search(input[1]);
+                }
+                else if (input[0].equals("clear"))
+                {
+                    stuManager.clear(input[1]);
+                }
+                else if (input[0].equals("print"))
+                {
+                    stuManager.print();
+                }
             }
         }
     }
